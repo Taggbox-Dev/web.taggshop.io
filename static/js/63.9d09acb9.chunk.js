a962216f13217dc071414075ab54397704f92e28
Vivek Sahu <109196868+VivekS30@users.noreply.github.com>
2024-02-01 10:53:37 +0530

    update

--- static/js/63.9d09acb9.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkcommerce_themes_update=self.webpackChunkcommerce_themes_update||[]).push([[63],{9063:(e,s,t)=>{t.r(s),t.d(s,{default:()=>o});var l=t(72791),a=t(2703),n=t(80184);const c=(0,l.lazy)((()=>Promise.all([t.e(809),t.e(276)]).then(t.bind(t,99276))));class r extends l.PureComponent{constructor(e){super(e),this.state={wall_id:(0,a.dR)()}}render(){const{wall_id:e}=this.state;return console.log(e),(0,n.jsx)(l.Suspense,{fallback:(0,n.jsx)(n.Fragment,{}),children:(0,n.jsx)(c,{wallID:e})})}}const o=r}}]);
-//# sourceMappingURL=63.9d09acb9.chunk.js.map
